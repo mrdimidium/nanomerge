be78876218e1578585f8e835edda17c85e9299b9
Nikolay Govorov <nikolay_govorov@bk.ru>
2018-07-03 16:11:02 +0300

    Added JSDoc from entry point

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var Merge = require("./merge");
 
 var merger = new Merge();
 
+/**
+ * Nanomerge wrapper
+ *
+ * @description - merges all the items passed to it into one using the default settings
+ * @params {[*]} - Items to merge
+ * @return {*} - merged item
+ */
 module.exports = function nanomerge() {
   return merger.merge.apply(merger, arguments);
 };
